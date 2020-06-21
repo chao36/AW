7f702b04473eb2b8a613671af177feb3c54bf34a
Akuma Walker <akuma.walker.0@gmail.com>
2020-06-21 15:41:39 +0300

    add logger test

---
+++ AW.BaseTests/Log/LoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AW.Base.Log;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AW.Base.Log.Tests
+{
+    [TestClass()]
+    public class LoggerTests
+    {
+        [TestMethod()]
+        public void LogTest()
+        {
+            Logger.Log("test");
+        }
+    }
+}
